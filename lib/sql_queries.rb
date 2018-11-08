62c2c1a73b2fa8a8c043b25dc2eae8ed6443271f
Merline Belizaire <merline@noukod.com>
2018-11-08 19:56:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,69 +30,10 @@ end
 
 def counts_number_of_bears_with_goofy_temperaments
 #  "Write your SQL query here"
-  "SELECT COUNT(temperament) FROM bears WHERE temperament='goofy';"  
+  "SELECT COUNT(temperament) FROM bears WHERE temperament='goofy';"
 end
 
 def selects_bear_that_killed_Tim
 #  "Write your SQL query here"
   "SELECT * FROM bears WHERE name IS NULL;"
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# def selects_all_female_bears_return_name_and_age
-#
-# end
-#
-# def selects_all_bears_names_and_orders_in_alphabetical_order
-#
-# end
-#
-# def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-#
-# end
-#
-# def selects_oldest_bear_and_returns_name_and_age
-#
-# end
-#
-# def select_youngest_bear_and_returns_name_and_age
-#
-# end
-#
-# def selects_most_prominent_color_and_returns_with_count
-#
-# end
-#
-# def counts_number_of_bears_with_goofy_temperaments
-#   
-# end
-#
-# def selects_bear_that_killed_Tim
-#
-# end
-#
